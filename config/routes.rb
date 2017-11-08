af970ac7ecd201383059174f46b095a9fd016806
Ubuntu <vagrant@Base-Box>
2017-11-08 20:48:35 +0000

    Added about us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'employee/index'
+  get 'employees', to: 'employee#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
