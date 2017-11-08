c1cf2ef70f75ae9126ecb4b243c53f62825f7777
Ubuntu <vagrant@Base-Box>
2017-11-08 05:58:50 +0000

    Added validate to Product and Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,6 @@ class Category < ApplicationRecord
 	has_many :products
 
 	validates :name, presence: true
-
+	validates :name, uniqueness: true
+	
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
 	belongs_to :category
 
 	validates :name, :price, :stock_quantity, presence: true
+	validates :name, uniqueness: true
 end
