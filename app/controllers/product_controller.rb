66ac314d5e907bf408980e48fcefbf83c8edbc22
Ubuntu <vagrant@Base-Box>
2017-11-21 15:59:03 +0000

    1, Added search bar at Shopping page; 2, Added functionality to search by category

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -3,9 +3,11 @@ class ProductController < ApplicationController
 		@rand_product = Product.order("RANDOM()").limit(4)
 	end
 
-	def products		
-		@categories = Category.all
-		@products = Product.all.page(params[:page]).per(9)
+	def products
+		if(!params[:key].nil? && !params[:category].nil?)
+			@categories = Category.all
+			@products = Product.where(category_id: params[:category]).page(params[:page]).per(9)
+		end
 	end
 
 	def product_by_category
