df5dd7dbde78cd304cad8ffc0cea815e52ded713
Ubuntu <vagrant@Base-Box>
2017-11-22 15:43:51 +0000

    Added search function associats to product and category

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -4,12 +4,20 @@ class ProductController < ApplicationController
 	end
 
 	def products
-		if(!params[:key].nil? && !params[:category].nil?)
-			@categories = Category.all
-			@products = Product.where(category_id: params[:category]).page(params[:page]).per(9)
-		end
-	end
+		@all_categories = Category.all
 
-	def product_by_category
+		if (params[:key].blank? && params[:category].blank?) # no search params
+			@categories = Category.all
+			@products = Product.all.page(params[:page]).per(9)
+		elsif (params[:key].blank? && !params[:category].blank?)
+			@categories = Category.where(id: params[:category])
+			@products = Product.where(category_id: params[:category]).page(params[:page]).per(9)
+		elsif (!params[:key].blank? && params[:category].blank?)
+			@categories = Category.all
+			@products = Product.where('name LIKE (?) OR description LIKE (?)', "%#{params[:key]}%", "%#{params[:key]}%").page(params[:page]).per(9)
+		elsif (!params[:key].blank? && !params[:category].blank?)
+			@categories = Category.where(id: params[:category])
+			@products = Product.where(category_id: params[:category]).where('name LIKE (?) OR description LIKE (?)', "%#{params[:key]}%", "%#{params[:key]}%").page(params[:page]).per(9)
+		end
 	end
 end
