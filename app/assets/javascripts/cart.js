4a0dabcb0a55bbc0179a13dcbb9d593a1b01b68c
Ubuntu <vagrant@Base-Box>
2017-11-27 06:52:44 +0000

    Added jQuery function to dynamic update item totle price

---
+++ app/assets/javascripts/cart.js
@@ -0,0 +1,28 @@
+var ready;
+var qty = 0;
+var price = 0;
+var subtotal =0;
+var estimatedTotal = 0;
+
+var MB_GST = 0.05;
+var MB_PST = 0.08;
+
+ready = function() {
+
+  var qty = 0;
+  var price = 0;
+  var subtotal =0;
+  var estimatedTotal = 0;
+  var cartItems = $(".cart-item");
+
+  $.each(cartItems, function( index, element){
+  	qty = $(this).find(".item-qty").val();
+  	price = $(this).find(".item-price").text();
+  	console.log(qty);
+  	console.log(price);
+
+  	$(this).find(".item-price").text("$ " + (qty * price).toFixed(2));
+  })
+};
+
+$(document).on('turbolinks:load', ready);
