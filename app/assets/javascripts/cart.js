b5f156c36b7fffd83d4c1575ec71b9074ff8a661
Ubuntu <vagrant@Base-Box>
2017-12-25 03:00:19 +0000

    Fixed an issue in cart javascript code

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -3,31 +3,33 @@ var MB_PST = 0.08;
 
 ready = function() {
 
-  var qty = 0;
-  var price = 0;
-  var subtotal = 0;
-  var estimatedTotal = 0;
-  var cartItems = $(".cart-item");
+  if($(".cart-item").length > 0){
+    var qty = 0;
+    var price = 0;
+    var subtotal = 0;
+    var estimatedTotal = 0;
+    var cartItems = $(".cart-item");
 
-  $.each(cartItems, function( index, element){
-  	qty = $(this).find(".item-qty").val();
-  	price = $(this).find(".item-price").text();
+    $.each(cartItems, function( index, element){
+      qty = $(this).find(".item-qty").val();
+      price = $(this).find(".item-price").text();
 
-  	$(this).find(".item-price").text("$ " + (qty * price).toFixed(2));
+      $(this).find(".item-price").text("$ " + (qty * price).toFixed(2));
 
-  	subtotal += qty * price;
+      subtotal += qty * price;
 
-  	if(qty == 1){
-  		$(this).find(".btn-danger").addClass("disabled");
-  	}
-  	
-  })
+      if(qty == 1){
+        $(this).find(".btn-danger").addClass("disabled");
+      }
+      
+    })
 
-  if($(".ship_address")["0"].innerText == "If Ship to Winnipeg"){
-    $(".sub_total").text("$ " + subtotal.toFixed(2));
-    $(".gst_amount").text((subtotal * MB_GST).toFixed(2));
-    $(".pst_amount").text((subtotal * MB_PST).toFixed(2));
-    $(".est_amount").text("$ " + (subtotal * (1 + MB_GST + MB_PST)).toFixed(2));
+    if($(".ship_address")["0"].innerText == "If Ship to Winnipeg"){
+      $(".sub_total").text("$ " + subtotal.toFixed(2));
+      $(".gst_amount").text((subtotal * MB_GST).toFixed(2));
+      $(".pst_amount").text((subtotal * MB_PST).toFixed(2));
+      $(".est_amount").text("$ " + (subtotal * (1 + MB_GST + MB_PST)).toFixed(2));
+    }
   }
 };
 
